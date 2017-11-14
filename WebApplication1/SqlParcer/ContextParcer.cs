a81c6b82cc74594dafe9730bfe2c6f4885859abe
serege <prototype9449@yandex.ru>
2017-11-14 23:58:10 +0300

    add files

--- WebApplication1/SqlParcer/ContextParcer.cs
+++ WebApplication1/SqlParcer/ContextParcer.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using DynamicWrapper;
+using System.Reflection.Emit;
 using Microsoft.SqlServer.Server;
 
 namespace SqlParcer
 {
     public static class ContextParcer
     {
+        private const string ConnectionString = "context connection=true";
+
         public class Tuple
         {
             public Tuple(string variable, string value)
@@ -19,117 +21,198 @@ namespace SqlParcer
             }
             public string Variable { get; set; }
             public string Value { get; set; }
-        }       
+        }
 
-        private static List<Tuple> GetIdentifiers(string identifierRow)
+        public enum VariableType
         {
-            var keyValues = identifierRow.Split(new[] { ',', ' ', '[', ']', '=' }, StringSplitOptions.RemoveEmptyEntries);
-            if(keyValues.Length % 2 != 0)
-            {
-                throw new Exception("should not be odd");
-            }
-            var result = new List<Tuple>();
+            Context,
+            Row,
+            Constant
+        }
 
-            for (var i = 0; i < keyValues.Length; i+=2)
+        public class Point
+        {
+            public VariableType Type { get; }
+            public string Value { get; }
+
+            public Point(VariableType type, string value)
             {
-                result.Add(new Tuple(keyValues[i], keyValues[i + 1]));
+                Type = type;
+                Value = value;
+            }
+
+            public static Point Parse(string row)
+            {
+                if (row.StartsWith("C."))
+                {
+                    return new Point(VariableType.Context, row.Substring(2));
+                }
+                else if (row.StartsWith("R."))
+                {
+                    return new Point(VariableType.Row, row.Substring(2));
+                }
+                else
+                {
+                    return new Point(VariableType.Constant, row);
+                }
+            }
+        }
+
+        public class Predicate
+        {
+            public Point Left { get; }
+            public Point Right { get; }
+
+            public Predicate(Point left, Point right)
+            {
+                Left = left;
+                Right = right;
+            }
+        }
+
+        public class Identfier
+        {
+            public string Column { get; }
+            public string Value { get; }
+            public string Type { get; }
+
+            public Identfier(string column, string value, string type)
+            {
+                Column = column;
+                Value = value;
+                Type = type;
+            }
+        }
+
+        public class SqlResult
+        {
+            public object Value { get; }
+            public Type ValueType { get; }
+
+            public SqlResult(object value, Type valueType)
+            {
+                Value = value;
+                ValueType = valueType;
+            }
+        }
+
+        private static List<Identfier> GetIdentifiers(string identifierRow)
+        {
+            var keyValues = identifierRow.Split(new[] { '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
+            if (keyValues.Length % 3 != 0)
+            {
+                throw new Exception("should be 3 values in a row");
+            }
+            var result = new List<Identfier>();
+
+            for (var i = 0; i < keyValues.Length; i += 3)
+            {
+                result.Add(new Identfier(keyValues[i], keyValues[i + 1], keyValues[i + 2]));
             }
             return result;
         }
 
-        private static List<Tuple> GetPredicates(string expressions)
+        private static List<Predicate> GetPredicates(string expressions)
         {
             var predicates = expressions.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-            var predicateTuples = new List<Tuple>();
+            var predicateTuples = new List<Predicate>();
             foreach (var pred in predicates)
             {
-                var values = pred.Split(new[] { '=', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                predicateTuples.Add(new Tuple(values[0], values[1]));
+                var values = pred.Split(new[] { '='}, StringSplitOptions.RemoveEmptyEntries);
+                predicateTuples.Add(new Predicate(Point.Parse(values[0]), Point.Parse(values[1])));
             }
 
             return predicateTuples;
         }
 
+        private static List<string> GetColumns(List<Predicate> predicates, VariableType variableType)
+        {
+            return predicates
+                .Where(x => x.Left.Type == variableType)
+                .Select(x => x.Left.Value)
+                .Union(
+                    predicates
+                        .Where(x => x.Right.Type == variableType)
+                        .Select(x => x.Right.Value)
+                ).ToList();
+        }
+
+        private static Dictionary<string, SqlResult> GetRowValues(string sqlEntityName, List<Identfier> identifiers, List<string> columns,
+            string connectionString = ConnectionString)
+        {
+            var whereStatement = string.Join(" and ", identifiers.Select(x => $"{x.Column} = @{x.Column}"));
+            var columnString = string.Join(", ", columns);
+            var sqlstringRequest = $"select {columnString} from {sqlEntityName} where {whereStatement}";
+
+            var result = new Dictionary<string, SqlResult>();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(sqlstringRequest, connection) { CommandType = CommandType.Text };
+                identifiers.ForEach(x =>
+                {
+                    object value;
+                    if (x.Type == "string")
+                    {
+                        value = x.Value;
+                    }
+                    else
+                    {
+                        value = int.Parse(x.Value);
+                    }
+
+                    var parameter = cmd.CreateParameter();
+                    parameter.Value = value;
+                    parameter.ParameterName = $"@{x.Column}";
+                    cmd.Parameters.Add(parameter);
+                });
+
+                cmd.Connection.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        for (var i = 0; i < reader.FieldCount; i++)
+                        {
+                            result.Add(reader.GetName(0), new SqlResult(reader.GetValue(i), reader.GetFieldType(i)));
+                        }
+                    }
+                }
+                finally
+                {
+                    cmd.Connection.Close();
+                    reader.Close();
+                }
+
+            }
+
+            return result;
+
+        }
+
         [SqlFunction(DataAccess = DataAccessKind.Read)]
-        public static bool ExecutePredicate(string schema, string tableName, string expressions, string rowIdentfierKeys)
+        public static bool ExecutePredicate(string currentTableName, string contextTableName, string expressions, 
+            string rowIdentfierKeys, string contextIdentifierKeys)
         {
             //1) parse expressions into separate predicates
             //2) parse each predicate to know what columns from context row are used and columns from current row
             //3) make 2 request - for current row, for context row
             //4) take data and 
 
-            var predicateTuples = GetPredicates(expressions);
-            var identifiers = GetIdentifiers(rowIdentfierKeys);
+            var predicates = GetPredicates(expressions);
 
-            var rowValues = rowKeys.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-            var rowTuples = new List<Tuple>();
-            foreach (var value in rowValues)
-            {
-                var values = value.Split(new[] { '=', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                rowTuples.Add(new Tuple(values[0], values[1].Trim('"')));
-            }
+            var rowIdentifiers = GetIdentifiers(rowIdentfierKeys);
+            var rowColumns = GetColumns(predicates, VariableType.Row);
 
-            var contextDict = predicateTuples.Where(x => x.Value.IndexOf('"') == -1).ToDictionary(t => t.Value, y => "");
-            var rowDict = rowTuples.ToDictionary(x => x.Variable, y => y.Value);
+            var contextIdentifers = GetIdentifiers(contextIdentifierKeys);
+            var contextColumns = GetColumns(predicates, VariableType.Context);
 
-            var contextQuery = "select " + string.Join(", ", contextDict.Keys.Select(x => string.Format("SESSION_CONTEXT(N'{0}') as {0}", x)));
-            if (contextDict.Keys.Count != 0)
-            {
-                using (SqlConnection connection = new SqlConnection("context connection=true"))
-                {
-                    SqlCommand cmd = new SqlCommand();
-                    SqlDataReader reader;
-
-                    cmd.CommandText = contextQuery;
-                    cmd.CommandType = CommandType.Text;
-                    cmd.Connection = connection;
-                    cmd.Connection.Open();
-                    reader = cmd.ExecuteReader();
-
-                    try
-                    {
-                        while (reader.Read())
-                        {
-                            contextDict = contextDict.Keys.ToDictionary(x => x, y => reader[y].ToString());
-                        }
-                    }
-                    finally
-                    {
-                        cmd.Connection.Close();
-                        reader.Close();
-                    }
-                }
-            }
+            var rowValues = GetRowValues($"{currentTableName}", rowIdentifiers, rowColumns);
+            var contextValues = GetRowValues($"{contextTableName}", contextIdentifers, contextColumns);
             
-            foreach (var keyValue in predicateTuples)
-            {
-                var key = "";
-                if (keyValue.Variable.IndexOf('"') == -1)
-                {
-                    key = rowDict[keyValue.Variable];
-                }
-                else
-                {
-                    key = keyValue.Variable.Trim('"');
-                }
-                var value = "";
-                if (keyValue.Value.IndexOf('"') == -1)
-                {
-                    value = contextDict[keyValue.Value];
-                }
-                else
-                {
-                    value = keyValue.Value.Trim('"');
-                }
 
-                if (key != value)
-                {
-                    return false;
-                }
-
-            }
-
-            return true;
         }
     }
 
